371b931cbcf4dbe818f0d74fc2708fc13bf40730
Danilo Tecla T <danilo.sampaio@teclat.com.br>
2021-10-10 15:56:50 -0300

    resposta teste

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,32 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "../components/MovieCard";
+
+interface MoviePropsMain {
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentPropsMain {
+  movies: MoviePropsMain[];
+}
+
+export function Content({ movies }: ContentPropsMain) {
+  return (
+    <main>
+      <div className="movies-list">
+        {movies.map((movie) => (
+          <MovieCard
+            title={movie.Title}
+            poster={movie.Poster}
+            runtime={movie.Runtime}
+            rating={movie.Ratings[0].Value}
+          />
+        ))}
+      </div>
+    </main>
+  );
 }
